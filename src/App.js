09b9499c7eb550327316eea4e132695cc6e37492
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-12-14 22:23:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-       <h1> Hello </h1>
+       <NavBar />
     </div>
   );
 }
