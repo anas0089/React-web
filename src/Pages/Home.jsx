a1113faa1a4edf80daeeb5f99bef48154193d885
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-12-16 16:43:37 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
 
+import NavBar from '../Components/NavBar/NavBar';
+
+
+function Home(props) {
+
+  return (
+
+    <div className="homeParentDiv">
+    <NavBar />
+    </div>
+
+  );
+
+}
+
+export default Home;
+
+ 
