5b35cdc2d063d02aa159884fe2bcfd320baca436
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-12-18 19:43:20 +0530

    Update NavBar.jsx

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,23 +1,23 @@
 <div className="NavBar" >
 		<div className="Logoplace" >
 			<a href="/" >
-				<img src="https://telegra.ph/file/31d2524903d8bea2552fc.jpg">
+				<img src="https://telegra.ph/file/31d2524903d8bea2552fc.jpg" />
 			</a>
 			<div className="catacry" >
-				<a href="/" >
+				<a href="/movies" >
 				Movies
 				</a>
-				<a href="/" >
+				<a href="/series" >
 				Series
 				</a>
-				<a href="/" >
+				<a href="/comedy" >
 				Comedy
 				</a>
 			</div>
 		</div>
 		<div className="serchbox" >
 			<form className="example" action="action_page.php">
-			<input type="text" placeholder="Search.." name="search" required>
+			<input type="text" placeholder="Search.." name="search" required />
 			<button type="submit"><i class="fa fa-search"></i></button>
 			</form>
 		</div>
