ecfa518e23928bc2ee21a3990d8c171d5a1ffe6c
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-12-21 20:50:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Home from './Pages/Home'
 function App() {
   return (
     <div className="App">
+     <Switch>
+      <Route path="/">
        <Home />
+      </Route>
+      <Route path="/movies">
+      </Route>
+     </Switch>
     </div>
   );
 }
