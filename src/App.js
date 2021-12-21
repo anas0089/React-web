7de2f2ae74e6cbd8b3ae61c1fb50c2b90788c520
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-12-21 20:53:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import './App.css'
 import Home from './Pages/Home'
+import {
+
+  BrowserRouter as Router,
+
+  Switch,
+
+  Route,
+
+  Link
+
+} from "react-router-dom";
 
 function App() {
   return (
